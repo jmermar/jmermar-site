92574d4613d65bdaea0abd581db27f6367cb117b
Jose Meroño <josesmerono@gmail.com>
2024-04-13 14:36:46 +0200

    Updated visuals

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,22 +13,20 @@ export default function Header() {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       ></link>
       <div className="header-content">
-        <h1>
-          <div className="centered">José Meroño Martínez</div>
-        </h1>
-        <h2>
-          <div className="centered">Personal site</div>
-        </h2>
+        <h1 className="pixelify-font centered">José Meroño Martínez's</h1>
+        <h2 className="pixelify-font centered">Personal site</h2>
         <nav>
           <div className="row centered">
-            <Link to="/" className="col nav-btn">
+            <Link to="/" className="col nav-btn pixelify-font">
               About
             </Link>
-            <Link to="/projects" className="col nav-btn">
+            <div className="separator"></div>
+            <Link to="/projects" className="col nav-btn pixelify-font">
               Projects
             </Link>
           </div>
         </nav>
+        <div className="header-separator centered"></div>
       </div>
     </header>
   );
