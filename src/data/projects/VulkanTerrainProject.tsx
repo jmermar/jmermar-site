246ba340f0eacc8c23b7aa3ce50cd31d96339ea2
Jose Meroño <josesmerono@gmail.com>
2024-06-02 16:25:29 +0200

    Updated terrain project

--- src/data/projects/VulkanTerrainProject.tsx
+++ src/data/projects/VulkanTerrainProject.tsx
@@ -24,96 +24,52 @@ const VulkanTerrainProject: ProjectTemplate = {
         <iframe
           width="800"
           height="450"
-          src={`https://www.youtube.com/embed/7c5PGedes9U`}
+          src={`https://www.youtube.com/embed/hysko2e0ro0`}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="Embedded youtube"
         />
       </div>
+      <p>Terrain renderer with dynamic LOD and SSR for water shading</p>
+      <h3> Dynamic LOD and procedural terrain</h3>
       <p>
-        Vulkan Forward procedural terrain renderer using tessellation shaders
-        for dynamic LOD and simplex noise for procedural heightmap generation.
+        There is a compute shader that will create quad patches surronding
+        player Field of View and dispatch them to the terrain rendering
+        pipeline.
       </p>
-      <p>It also supports Screen Space Reflection (SSR) for water shading.</p>
-      <h3> Tessellation shader </h3>
-      The tessellation shader works with quads called patches, each patch will
-      be subdivided in up to GPU max tessellation depending of the distance to
-      camera, once the quad have been tessellated each new vertex will gather
-      it's height from a noise function which uses simplex noise with multiple
-      <h3>Passes</h3>
+
       <p>
-        Currently the renderer consits of three passes, patch generation, depth
-        prepass and forward rendering. For each patch generated, it will also
-        create a quad serving as water surface for latter passes.
+        Each patch will pass by a tessellation shader that will subdivide it
+        depending of its distance to player camera, thus achieving dynamic LOD.
       </p>
-      <h4> Patch generation</h4>
       <p>
-        This phase will write all patches that will be tessellated to a vertex
-        buffer, applying frustum culling to those which are not visible.
+        The tessellation evaluation will set an height for each of the
+        tessellated vertices depending of a perlin noise function by using the x
+        and z world coordinates as input.
       </p>
-      <h4> Depth Preppass </h4>
-      <p>
-        This phase will draw the terrain just to the depth buffer, it is
-        intended for future use with techniques such as forward+ rendering.
-      </p>
-      <h4>Forward rendering</h4>
-      <p>
-        This phase will read the precalculated depth buffer but not write onto
-        it, and will have a fragment shader with will output to the screen
-        framebuffer only. In this fragment the color is calculated by looking at
-        different textures depending of the terrain height and applying diffuse
-        ligthing from a directional light source.
-      </p>
-      <h4>Water rendering</h4>
+      <h3>Water shading</h3>
       <img src={waterGif} />
       <p>
-        The water shading is highly inspired by{" "}
+        Water will be composed of chunks of quads with only 4 vertices, each
+        quad will be shaded as explained in{" "}
         <Link to="https://www.youtube.com/watch?v=HusvGeEDU_U&list=PLRIWtICgwaX23jiqVByUs0bqhnalNTNZh">
-          this series of videos
+          these video series
         </Link>
-        . However, in order to not draw the scene two more times screen space
-        reflection have been used both for reflection and refraction.
+        . It mainly consists of both offset and normal textures for managing
+        reflections and refraction as well as Schlick approximation for Fresnel
+        equation and Bling Phong specular lighting.
       </p>
       <p>
-        Before rendering the water, the framebuffer is copied to an image that
-        will be used as a texture by the water fragment shader, and in the water
-        shader a reflection ray is casted over the water surface and traced
-        until hits with and object by using the depth buffer of the image. Once
-        hit the ray is transformed to screen coordinates and will gather the
-        reflected color from the screen texture. This have the advantage of not
-        needing to redraw the scene multiple times nor using raytracing over
-        complex scenes, but will cause visual artifacts when a surface reflects
-        a part of the scene which have not been rendered.
-      </p>
-      <p>
-        We need to cast 2 rays, one for the reflected rays over the water
-        surface and other for the refracted, this will give 2 color outputs that
-        will be blended by a factor roughly inspired by the fresnel equation
-        (the more perpendicular the view vector is to the water surface, the
-        less rays are reflected and instead more are refracted).
-      </p>
-      <p>
-        Finally, before getting the color from the screen texture, the
-        calculated uv coordinates are offsetted by a texture for simulating a
-        wave effect. This texture is called dudv map and contain offsets for v
-        and u coordinates.
-      </p>
-      <p>
-        {" "}
-        The final result is a rough approximation of how water works in real
-        life, wy margin of improvement by adding Physically Based techniques to
-        the proccess
-      </p>
-      <p>
-        An explanation of the SSR method can be found{" "}
+        Instead of redrawing the scene two more times SSR have been use for
+        water reflections, as explained{" "}
         <Link to="https://lettier.github.io/3d-game-shaders-for-beginners/screen-space-reflection.html">
           here
         </Link>
         .
       </p>
       <p>
-        More info on the project can be found in it's{" "}
+        Source code available in it's{" "}
         <Link to={projectPath}>github repository</Link>.
       </p>
     </>
